43f5b485bf3b53da9c55cb69abcdc3d3b6e0fb5e
MGHMSJ5 <elise.koezema@hotmail.com>
2022-10-06 13:08:41 +0200

    The Script with the Random Object Spawner
    Put the script in, made a Prefab folder, with two objects (cube, sphere)

---
+++ Assets/TheGame/Scripts/RandomObjSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomObjSpawner : MonoBehaviour
+{
+  public GameObject[] myObjects;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            int RandomIndex=Random.Range(0, myObjects.Length);
+            Vector3 randomSpawnPosition=new Vector3(Random.Range(-10,11), 1, Random.Range(-10,11));
+
+            Instantiate(myObjects[RandomIndex], randomSpawnPosition, Quaternion.identity);
+        }
+    }
+}
