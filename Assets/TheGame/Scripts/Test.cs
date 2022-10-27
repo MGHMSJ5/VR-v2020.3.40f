d3b1cde6b5e33f3168ae34aae6da029880a1115d
MGHMSJ5 <elise.koezema@hotmail.com>
2022-10-27 14:45:20 +0200

    ChooseTherapistPatient

--- Assets/TheGame/Scripts/Test.cs
+++ Assets/TheGame/Scripts/Test.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     public GameObject cube;
+
+    //new
+    MenuCode menuCode;
+    [SerializeField] GameObject menuScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        menuCode = menuScript.GetComponent<MenuCode>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.O))
+        if (menuCode.checkBool)
         {
-            cube.SetActive(true);
-            Debug.Log("ooooooooooo");
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                cube.SetActive(true);
+                Debug.Log("ooooooooooo");
+            }
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                cube.SetActive(false);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            cube.SetActive(false);
-        }
+
     }
 }
