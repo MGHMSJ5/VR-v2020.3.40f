2160012efd23574ff399dff9000234de60bec4c1
MGHMSJ5 <elise.koezema@hotmail.com>
2022-11-07 10:52:15 +0100

    Grab Interaction

---
+++ Assets/TheGame/Scripts/grabRequest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class grabRequest : MonoBehaviour
+{
+
+    private RealtimeTransform realtimeTransform;
+    private XRGrabInteractable xRGrabInteractable;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        realtimeTransform = GetComponent<RealtimeTransform>();
+        xRGrabInteractable = GetComponent<XRGrabInteractable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (xRGrabInteractable.isSelected) 
+        {
+            realtimeTransform.RequestOwnership();
+        }
+    }
+}
