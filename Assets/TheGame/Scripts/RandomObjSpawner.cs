a023fb194bf9a55f67569b808c9d09bf2ae532e5
MGHMSJ5 <elise.koezema@hotmail.com>
2022-11-07 18:11:30 +0100

    ???

--- Assets/TheGame/Scripts/RandomObjSpawner.cs
+++ Assets/TheGame/Scripts/RandomObjSpawner.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Normal.Realtime;
 
 public class RandomObjSpawner : MonoBehaviour
 {
   public GameObject[] myObjects;
+    private string cubeName;
 
     
     private void Start()
@@ -22,11 +24,14 @@ public class RandomObjSpawner : MonoBehaviour
         }
     }
 
+    [System.Obsolete]
     public void SpawnObjects()
     {
         int RandomIndex = Random.Range(0, myObjects.Length);
         Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 0, Random.Range(-10, 11));
+        cubeName = myObjects[RandomIndex].name;
 
-        Instantiate(myObjects[RandomIndex], randomSpawnPosition, Quaternion.identity);
+        //Instantiate(myObjects[RandomIndex], randomSpawnPosition, Quaternion.identity);
+        Realtime.Instantiate(cubeName);
     }
 }
