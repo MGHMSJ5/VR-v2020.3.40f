43216a9646ffcdb32a64b05e4bb78d13dbfc5aa1
MGHMSJ5 <elise.koezema@hotmail.com>
2022-11-09 11:20:00 +0100

    bad fix

--- Assets/TheGame/Scripts/RandomObjSpawner.cs
+++ Assets/TheGame/Scripts/RandomObjSpawner.cs
@@ -7,7 +7,7 @@ public class RandomObjSpawner : MonoBehaviour
 {
     public GameObject[] goodObjects; //list with all the Spawners
 
-    public GameObject[] badObjects;
+    public GameObject[] badObjects; //list with the bad objects
 
     private string prefabName; //ame of the selected prefab in the spawners
 
@@ -47,8 +47,8 @@ public class RandomObjSpawner : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.B) && menuCode.checkBool || BNG.InputBridge.Instance.XButton && menuCode.checkBool)
             //BNG.InputBridge.Instance.XButton
             {
-                startLocation = goodObjects[SpawnCounter].GetComponent<StartLocation>();
-                prefabName = goodObjects[SpawnCounter].name;
+                startLocation = badObjects[SpawnCounter].GetComponent<StartLocation>();
+                prefabName = badObjects[SpawnCounter].name;
                 startLocation.activateBad = true;
                 Realtime.Instantiate(prefabName);
                 SpawnCounter++;
